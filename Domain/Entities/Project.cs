dd9248709b227123f57fbe063a3c8e96a263da8a
unknown <georges19232@gmail.com>
2023-04-15 23:11:07 -0700

    Implemented Domain

---
+++ Domain/Entities/Project.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Project : BaseAuditableEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Bpm { get; set; }
+        public string? TimeSig { get; set; }
+    }
+}
