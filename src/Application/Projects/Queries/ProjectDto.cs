f469df62298e46cfa9fe4d8151feab3ffa6bbd68
unknown <georges19232@gmail.com>
2023-04-16 13:48:13 -0700

    Filled out missing code for the new projects

---
+++ src/Application/Projects/Queries/ProjectDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Projects.Queries;
+
+public class ProjectDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int Bpm { get; set; }
+    public string? TimeSig { get; set; }
+}
