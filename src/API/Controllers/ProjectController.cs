a8ed4a8c9157b5aa829f0917905f7994c788e158
unknown <georges19232@gmail.com>
2023-05-06 10:28:31 -0700

    Added track endpoint and updated swagger

--- src/API/Controllers/ProjectController.cs
+++ src/API/Controllers/ProjectController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Projects.Queries;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers;
@@ -17,22 +18,31 @@ public class ProjectController : ControllerBase
 
     // GET: api/<ProjectController>
     [HttpGet]
+    [ProducesResponseType(typeof(List<ProjectDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Get()
     {
         var result = await _mediator.Send(new GetProjectsQuery());
+
+        if (!result.Any())
+        {
+            return NotFound("Projects Not Found");
+        }
 
         return Ok(result);
     }
 
     // GET: api/<ProjectController>
     [HttpGet("{id}")]
+    [ProducesResponseType(typeof(ProjectDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Get([FromRoute] Guid id)
     {
         var result = await _mediator.Send(new GetProjectQuery(id));
 
         if (result == null)
         {
-            return BadRequest("Project not found.");
+            return NotFound("Project not found.");
         }
 
         return Ok(result);
---
+++ src/Application/Tracks/Queries/GetTracksQuery.cs
@@ -0,0 +1,32 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Tracks.Queries;
+
+public record GetTracksQuery(Guid ProjectId) : IRequest<List<TrackDto>>;
+
+public class GetTracksQueryHandler : IRequestHandler<GetTracksQuery, List<TrackDto>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetTracksQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<TrackDto>> Handle(GetTracksQuery request, CancellationToken cancellationToken)
+    {
+        var tracksList = await _context.Tracks
+            .Where(t => t.ProjectId == request.ProjectId)
+            .ToListAsync();
+
+        return tracksList.Select(t => new TrackDto
+        {
+            Id = t.Id,
+            Name = t.Name,
+            ImageUrl = t.ImageUrl,
+            ProjectId = t.ProjectId
+        }).ToList();
+    }
+}
