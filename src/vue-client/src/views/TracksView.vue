58272f403521172951f00ed67f93e4ebb2262c24
unknown <georges19232@gmail.com>
2023-06-04 15:25:43 -0700

    Handled project not found exception

--- src/vue-client/src/views/TracksView.vue
+++ src/vue-client/src/views/TracksView.vue
@@ -11,16 +11,23 @@ const router = useRouter();
 const projectId = route.params.projectId as string;
 
 onBeforeMount(async () => {
+    try {
+        projectMeta.value = await Project.getProject(projectId);
+    }
+    catch {
+        return router.push('/NotFound')
+    }
+
     tracks.value = await Track.getTracks(projectId);
-    projectMeta.value = await Project.getProject(projectId);
 });
+
 </script>
 
 <template>
     <VContainer>
         <VRow class="flex-nowrap mb-2">
             <VCol class="flex-grow-1 flex-shrink-0">
-                <h2> <span class="text-grey-lighten-1" @click="router.push('/')">Projects</span> / {{ projectMeta?.name }} </h2>
+                <h2><span class="text-grey-lighten-1" @click="router.push('/')">Projects</span> / {{ projectMeta?.name }}</h2>
             </VCol>
             <VCol cols="1">
                 <VBtn :elevation="1" :border="true">
@@ -29,16 +36,20 @@ onBeforeMount(async () => {
                 </VBtn>
             </VCol>
         </VRow>
-        <VList lines="one">
+        <VList lines="one" v-if="tracks">
             <VListItem
                 v-for="track in tracks"
-                :key="track.id"
+                :key="track.id" 
                 :title="track.name"
                 class="ma-1 pb-5 pt-5"
                 :border="true"
                 :elevation="2"
                 :rounded="true"
-            ></VListItem>
+            >
+            </VListItem>
         </VList>
+        <VCard v-else>
+            No tracks ... yet!
+        </VCard>
     </VContainer>
 </template>
