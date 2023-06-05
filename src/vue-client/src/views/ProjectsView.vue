7482eb8024618c479be11bc42ec4b5c6fdfe7156
unknown <georges19232@gmail.com>
2023-06-04 19:50:13 -0700

    fixed styling

--- src/vue-client/src/views/ProjectsView.vue
+++ src/vue-client/src/views/ProjectsView.vue
@@ -1,40 +1,44 @@
 <script setup lang="ts">
-import { Ref, ref, onBeforeMount } from 'vue';
+import { Ref, ref } from 'vue';
 import { IProject, Project } from '../api';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
 const projects: Ref<IProject[]> = ref([]);
 
-onBeforeMount(async () => {
-  projects.value = await Project.getProjects();
+Project.getProjects()
+.then(data => {
+    projects.value = data;
 });
+
 </script>
 
 <template>
     <VContainer>
-        <VRow class="flex-nowrap mb-2">
-            <VCol class="flex-grow-1 flex-shrink-0">
+        <VRow class="d-flex mb-6">
+            <VCol class="ma-2 pa-2 me-auto">
                 <h2>Projects</h2>
             </VCol>
-            <VCol cols="1">
+            <VCol class="d-flex justify-end ma-2 pa-2">
                 <VBtn :elevation="1" :border="true">
                     Add
                     <VIcon end icon="mdi-plus" class="pr-2" />
                 </VBtn>
             </VCol>
         </VRow>
-        <VList lines="one">
-            <VListItem
-                v-for="project in projects"
-                :key="project.id"
-                :title="project.name"
-                @click="router.push(`/tracks/${project.id}`)"
-                class="ma-1 pb-5 pt-5"
-                :border="true"
-                :elevation="2"
-                :rounded="true"
-            ></VListItem>
-        </VList>
+        <VRow>
+            <VList class="w-100" lines="one">
+                <VListItem
+                    v-for="project in projects"
+                    :key="project.id"
+                    :title="project.name"
+                    @click="router.push(`/tracks/${project.id}`)"
+                    class="ma-1 pb-5 pt-5"
+                    :border="true"
+                    :elevation="2"
+                    :rounded="true"
+                ></VListItem>
+            </VList>
+        </VRow>
     </VContainer>
 </template>
--- src/vue-client/src/views/TracksView.vue
+++ src/vue-client/src/views/TracksView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useRoute, useRouter } from 'vue-router';
-import { Ref, ref, onBeforeMount } from 'vue';
+import { Ref, ref } from 'vue';
 import { ITrack, Track, IProjectMetaData, Project } from '../api';
 
 const projectMeta: Ref<IProjectMetaData | null> = ref(null);
@@ -10,26 +10,26 @@ const router = useRouter();
 
 const projectId = route.params.projectId as string;
 
-onBeforeMount(async () => {
-    try {
-        projectMeta.value = await Project.getProject(projectId);
-    }
-    catch {
-        return router.push('/NotFound')
-    }
+Project.getProject(projectId)
+.then(data => {
+    projectMeta.value = data;
+}).catch(() => {
+    router.push('/NotFound');
+})
 
-    tracks.value = await Track.getTracks(projectId);
+Track.getTracks(projectId)
+.then(data => {
+    tracks.value = data;
 });
-
 </script>
 
 <template>
     <VContainer>
-        <VRow class="flex-nowrap mb-2">
-            <VCol class="flex-grow-1 flex-shrink-0">
+        <VRow class="d-flex mb-6">
+            <VCol class="ma-2 pa-2 me-auto">
                 <h2><span class="text-grey-lighten-1" @click="router.push('/')">Projects</span> / {{ projectMeta?.name }}</h2>
             </VCol>
-            <VCol cols="1">
+            <VCol class="d-flex justify-end ma-2 pa-2">
                 <VBtn :elevation="1" :border="true">
                     Add
                     <VIcon end icon="mdi-plus" class="pr-2" />
